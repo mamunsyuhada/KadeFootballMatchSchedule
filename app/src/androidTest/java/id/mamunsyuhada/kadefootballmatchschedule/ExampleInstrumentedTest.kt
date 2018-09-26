452270bbf01ea0dcf072f5df60cb3370a3bf9e14
mamunsyuhada <imam.sy11@gmail.com>
2018-09-27 01:29:02 +0700

    first commit

---
+++ app/src/androidTest/java/id/mamunsyuhada/kadefootballmatchschedule/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package id.mamunsyuhada.kadefootballmatchschedule
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("id.mamunsyuhada.kadefootballmatchschedule", appContext.packageName)
+    }
+}
